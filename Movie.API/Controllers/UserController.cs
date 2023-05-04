d714019354be95a658e18e4d47cf45dfbef8620c
elifboluk <elifbolukk@gmail.com>
2023-05-04 15:37:50 +0300

    Add project files.

---
+++ Movie.API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movie.Core.DTOs;
+using Movie.Core.Services;
+
+namespace Movie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : CustomBaseController
+    {
+        public readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        // Metodun tipine göre eşleşme
+        // api/user
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDto createUserDto) // Kullanıcı oluştur.
+        {            
+            return ActionResultInstance(await _userService.CreateUserAsync(createUserDto));
+        }
+
+        [Authorize] // Bu end point mutlaka bir token istiyor.
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            // HttpContext.User.Claims.Where(x=x.Type == "username");
+            return ActionResultInstance(await _userService.GetUserByNameAsync(HttpContext.User.Identity.Name));
+        }
+
+    }
+}
--- Movie.API/Program.cs
+++ Movie.API/Program.cs
@@ -81,7 +81,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
